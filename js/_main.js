c8d221b257c412cfd630ddc3c4c203863f6d5650
Dext4er0016 <nicschmid@hispeed.ch>
2017-11-06 22:16:47 +0100

    Added own pics

--- js/_main.js
+++ js/_main.js
@@ -25,9 +25,13 @@
 } )( jQuery );
 
 
-function getExif() {
-    var img1 = document.getElementById("hiddenImage");
+function getExif(imageId) {
+    alert("GetExif ImageId " + imageId);
+    var img1 = document.getElementById(imageId);
+    var myMap = new Map();
+    alert("Actual ImageID: " + img1.id);
     EXIF.getData(img1, function() {
+        alert("Inside of Method");
         var make = EXIF.getTag(this, "Make");
         var model = EXIF.getTag(this, "Model");
         var exposure = EXIF.getTag(this, "ExposureTime");
@@ -42,44 +46,63 @@ function getExif() {
         var imageHeight = EXIF.getTag(this, "ImageHeight");
         var compression = EXIF.getTag(this, "Compression");
 
-        var makeElement = document.getElementById("make");
-        var modelElement = document.getElementById("model");
-        var exposureElement = document.getElementById("exposure");
-        var fNumberElement = document.getElementById("fNumber");
-        var isoSpeedElement = document.getElementById("isoSpeed");
-        var flashElement = document.getElementById("flash");
-        var descriptionElement = document.getElementById("description");
-        var orientationElement = document.getElementById("orientation");
-        var softwareElement = document.getElementById("software");
-        var dateElement = document.getElementById("date");
-        var imageWidthElement = document.getElementById("imageWidth");
-        var imageHeightElement = document.getElementById("imageHeight");
-        var compressionElement = document.getElementById("compression");
-        var card = document.getElementById("exifCard1");
-        var card2 = document.getElementById("exifCard2");
-
-        card.style.display = "flex";
-        card2.style.display = "flex";
-
         if (!description.replace(/\s/g, '').length) {
             description="-";
         }
 
-        makeElement.innerHTML = make;
-        modelElement.innerHTML = model;
-        exposureElement.innerHTML = exposure;
-        fNumberElement.innerHTML = fNumber;
-        isoSpeedElement.innerHTML = isoSpeed;
-        flashElement.innerHTML = flash;
-        descriptionElement.innerHTML = description;
-        orientationElement.innerHTML = orientation;
-        softwareElement.innerHTML = software;
-        dateElement.innerHTML = date;
-        imageWidthElement.innerHTML = imageWidth;
-        imageHeightElement.innerHTML = imageHeight;
-        compressionElement.innerHTML = compression;
+        myMap.set("Make", make);
+        myMap.set("Model", model);
+        myMap.set("ExposureTime", exposure);
+        myMap.set("FNumber", fNumber);
+        myMap.set("ISOSpeedRatings", isoSpeed);
+        myMap.set("Flash", flash);
+        myMap.set("ImageDescription", description);
+        myMap.set("Orientation", orientation);
+        myMap.set("Software", software);
+        myMap.set("DateTimeOriginal", date);
+        myMap.set("ImageWidth", imageWidth);
+        myMap.set("ImageHeight", imageHeight);
+        myMap.set("Compression", compression);
 
     });
+    return myMap;
+}
+
+function showExifUpload(imageId){
+    var exifMap = getExif(imageId);
+    var makeElement = document.getElementById("make");
+    var modelElement = document.getElementById("model");
+    var exposureElement = document.getElementById("exposure");
+    var fNumberElement = document.getElementById("fNumber");
+    var isoSpeedElement = document.getElementById("isoSpeed");
+    var flashElement = document.getElementById("flash");
+    var descriptionElement = document.getElementById("description");
+    var orientationElement = document.getElementById("orientation");
+    var softwareElement = document.getElementById("software");
+    var dateElement = document.getElementById("date");
+    var imageWidthElement = document.getElementById("imageWidth");
+    var imageHeightElement = document.getElementById("imageHeight");
+    var compressionElement = document.getElementById("compression");
+    var card = document.getElementById("exifCard1");
+    var card2 = document.getElementById("exifCard2");
+
+    card.style.display = "flex";
+    card2.style.display = "flex";
+
+    makeElement.innerHTML = exifMap.get("Make");
+    modelElement.innerHTML = exifMap.get("Model");
+    exposureElement.innerHTML = exifMap.get("ExposureTime");
+    fNumberElement.innerHTML = exifMap.get("FNumber");
+    isoSpeedElement.innerHTML = exifMap.get("ISOSpeedRatings");
+    flashElement.innerHTML = exifMap.get("Flash");
+    descriptionElement.innerHTML = exifMap.get("ImageDescription");;
+    orientationElement.innerHTML = exifMap.get("Orientation");
+    softwareElement.innerHTML = exifMap.get("Software");
+    dateElement.innerHTML = exifMap.get("DateTimeOriginal");
+    imageWidthElement.innerHTML = exifMap.get("ImageWidth");
+    imageHeightElement.innerHTML = exifMap.get("ImageHeight");
+    compressionElement.innerHTML = exifMap.get("Compression");
+
 }
 
 
@@ -98,15 +121,16 @@ function readURLforExif(input)
         output.src = dataURL;
     };
     reader.readAsDataURL(input.files[0]);
-    getExif()
-
+    getExif("hiddenImage")
 }
 
 
 
 
 var imageLoader = document.getElementById('filePhoto');
-imageLoader.addEventListener('change', handleImage, false);
+if(imageLoader != null) {
+    imageLoader.addEventListener('change', handleImage, false);
+}
 
 function handleImage(e) {
     var reader = new FileReader();
@@ -116,4 +140,48 @@ function handleImage(e) {
         $('.cameraImage').attr('src',event.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
+}
+
+function showExifHome(divId, imageId){
+    alert(divId);
+    var bgDiv = $("#slide4Div").css("background-image");
+    res = bgDiv.substring(4, bgDiv.length -1);
+    $("#hiddenImage").attr("src",res);
+    //var output = document.getElementById('hiddenImage');
+    //output.src = res;
+
+    alert("Image: " + bgDiv);
+    alert("Result: " + res);
+    alert("Attribute: " + $("#hiddenImage").attr("src"));
+
+    var exifMap = getExif("hiddenImage");
+    var makeElement = document.getElementById("makeMain");
+    var modelElement = document.getElementById("modelMain");
+    var exposureElement = document.getElementById("exposureMain");
+    var fNumberElement = document.getElementById("fNumberMain");
+    var isoSpeedElement = document.getElementById("isoSpeedMain");
+    var flashElement = document.getElementById("flashMain");
+    var descriptionElement = document.getElementById("descriptionMain");
+    var orientationElement = document.getElementById("orientationMain");
+    var softwareElement = document.getElementById("softwareMain");
+    var dateElement = document.getElementById("dateMain");
+    var imageWidthElement = document.getElementById("imageWidthMain");
+    var imageHeightElement = document.getElementById("imageHeightMain");
+    var compressionElement = document.getElementById("compressionMain");
+
+    alert(exifMap.get("Make"));
+
+    makeElement.innerHTML = exifMap.get("Make");
+    modelElement.innerHTML = exifMap.get("Model");
+    exposureElement.innerHTML = exifMap.get("ExposureTime");
+    fNumberElement.innerHTML = exifMap.get("FNumber");
+    isoSpeedElement.innerHTML = exifMap.get("ISOSpeedRatings");
+    flashElement.innerHTML = exifMap.get("Flash");
+    descriptionElement.innerHTML = exifMap.get("ImageDescription");;
+    orientationElement.innerHTML = exifMap.get("Orientation");
+    softwareElement.innerHTML = exifMap.get("Software");
+    dateElement.innerHTML = exifMap.get("DateTimeOriginal");
+    imageWidthElement.innerHTML = exifMap.get("ImageWidth");
+    imageHeightElement.innerHTML = exifMap.get("ImageHeight");
+    compressionElement.innerHTML = exifMap.get("Compression");
 }
